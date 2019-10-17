d04b7da7451555c62f0eae5f81deaa89ffe18c7a
xioahutu <1328669873@qq.com>
2019-10-17 16:05:34 +0800

    增加.ignore

--- src/main/java/xiaohutu/java/App.java
+++ src/main/java/xiaohutu/java/App.java
@@ -1,13 +1,40 @@
 package xiaohutu.java;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String webPath = System.getProperty("webroot.path");
+        System.out.println(webPath);
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("user.dir"));
+//        for(int i=0;i<10;i++){
+//            System.out.println(UUID.randomUUID().toString());
+//        }
+        String property = System.getProperty("user.dir");
+        System.out.println(property);
+        Long nextValue = 4L;
+        Long increaseValue = 1L;
+        System.out.println(nextValue - increaseValue);
+        List<vo> list = new ArrayList<>();
+        String collect = list.stream().map(vo::getName).collect(Collectors.joining(","));
+        System.out.println("collect:" + collect);
+
     }
+
+    @Data
+    public class vo {
+        private String name;
+        private String value;
+    }
+
 }
